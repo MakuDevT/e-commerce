d44c2e2bcd99b0c66719394ea56063d6d3a6f6fa
MakuDevT <markangelo.u.unida@gmail.com>
2023-04-21 23:58:12 +0800

    added Riverpod State Management

--- lib/src/features/products/data/fake_products_repository.dart
+++ lib/src/features/products/data/fake_products_repository.dart
@@ -1,13 +1,11 @@
 import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../domain/product.dart';
 
 class FakeProductsRepository {
   //Future are for REST API
   //Stream are for real time
-  FakeProductsRepository._(); // private constructor
-  //singleton
-  static FakeProductsRepository instance = FakeProductsRepository._();
 
   final List<Product> _products = kTestProducts;
 
@@ -19,16 +17,38 @@ class FakeProductsRepository {
     return _products.firstWhere((product) => product.id == id);
   }
 
-  Future<List<Product>> fetchProductsList() {
+  Future<List<Product>> fetchProductsList() async {
+    await Future.delayed(const Duration(seconds: 2));
+    // throw Exception("Error Error Error");
     return Future.value(_products);
   }
 
-  Stream<List<Product>> watchProductList() {
-    return Stream.value(_products);
+  Stream<List<Product>> watchProductsList() async* {
+    await Future.delayed(const Duration(seconds: 2));
+    // return Stream.value(_products);
+    yield _products;
   }
 
   Stream<Product?> watchProduct(String id) {
-    return watchProductList()
+    return watchProductsList()
         .map((products) => products.firstWhere((product) => product.id == id));
   }
 }
+
+final productsRepositoryProvider = Provider<FakeProductsRepository>((ref) {
+  return FakeProductsRepository();
+});
+
+final productsListStreamProvider = StreamProvider<List<Product>>((ref) {
+  final productRepository = ref.watch(productsRepositoryProvider);
+  return productRepository.watchProductsList();
+});
+final productsListFutureProvider = FutureProvider<List<Product>>((ref) {
+  final productRepository = ref.watch(productsRepositoryProvider);
+  return productRepository.fetchProductsList();
+});
+
+final productProvider = StreamProvider.family<Product?, String>((ref, id) {
+  final productRepository = ref.watch(productsRepositoryProvider);
+  return productRepository.watchProduct(id);
+});
