c2ad7f0865b3204987176bce4f6f001c287ff123
MakuDevT <markangelo.u.unida@gmail.com>
2023-04-25 23:43:16 +0800

    added controllers for authentication feature

--- lib/src/features/authentication/presentation/account/account_screen.dart
+++ lib/src/features/authentication/presentation/account/account_screen.dart
@@ -1,38 +1,58 @@
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen_controller.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/action_text_button.dart';
 import 'package:ecommerce_app/src/common_widgets/responsive_center.dart';
 import 'package:ecommerce_app/src/constants/app_sizes.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 /// Simple account screen showing some user info and a logout button.
-class AccountScreen extends StatelessWidget {
+class AccountScreen extends ConsumerWidget {
   const AccountScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<AsyncValue<void>>(accountScreenControllerPRovider,
+        (previousState, state) {
+      if (state.isLoading && state.hasError) {
+        showExceptionAlertDialog(
+            context: context, title: 'Error'.hardcoded, exception: state.error);
+      }
+    });
+    final state = ref.watch(accountScreenControllerPRovider);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Account'.hardcoded),
+        title: state.isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : Text('Account'.hardcoded),
         actions: [
           ActionTextButton(
             text: 'Logout'.hardcoded,
-            onPressed: () async {
-              // showNotImplementedAlertDialog(context: context);
-              final navigator = Navigator.of(context);
-              final logout = await showAlertDialog(
-                context: context,
-                title: 'Are you sure?'.hardcoded,
-                cancelActionText: 'Cancel'.hardcoded,
-                defaultActionText: 'Logout'.hardcoded,
-              );
-              if (logout == true) {
-                // TODO: Sign out the user.
-                navigator.pop();
-              }
-            },
+            onPressed: state.isLoading
+                ? null
+                : () async {
+                    // showNotImplementedAlertDialog(context: context);
+
+                    final logout = await showAlertDialog(
+                      context: context,
+                      title: 'Are you sure?'.hardcoded,
+                      cancelActionText: 'Cancel'.hardcoded,
+                      defaultActionText: 'Logout'.hardcoded,
+                    );
+                    if (logout == true) {
+                      final success = await ref
+                          .read(accountScreenControllerPRovider.notifier)
+                          .signOut();
+                      if (success) {
+                        Navigator.pop(context);
+                      }
+                      // navigator.pop();
+                    }
+                  },
           ),
         ],
       ),
---
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -0,0 +1,27 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AccountScreenController extends StateNotifier<AsyncValue<void>> {
+  AccountScreenController({required this.authRepository})
+      : super(const AsyncValue<void>.data(null));
+
+  final FakeAuthRepository authRepository;
+
+  Future<bool> signOut() async {
+    try {
+      state = const AsyncValue<void>.loading();
+      await authRepository.signOut();
+      state = const AsyncValue<void>.data(null);
+      return true;
+    } catch (e, st) {
+      state = AsyncValue<void>.error(e, st);
+      return false;
+    }
+  }
+}
+
+final accountScreenControllerPRovider = StateNotifierProvider.autoDispose<
+    AccountScreenController, AsyncValue<void>>((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return AccountScreenController(authRepository: authRepository);
+});
