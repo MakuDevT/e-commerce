54dbd31e6626e2fd5295ae407a5e07b70cf17254
MakuDevT <markangelo.u.unida@gmail.com>
2023-06-01 15:05:25 +0800

    added riverpod generator and asyncnotif test

--- lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -1,21 +1,19 @@
+import 'dart:async';
+
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class AccountScreenController extends StateNotifier<AsyncValue<void>> {
-  AccountScreenController({required this.authRepository})
-      : super(const AsyncData(null));
-  final FakeAuthRepository authRepository;
+part 'account_screen_controller.g.dart';
 
+@riverpod
+class AccountScreenController extends _$AccountScreenController {
+  @override
+  FutureOr<void> build() {
+    // nothing to do
+  }
   Future<void> signOut() async {
+    final authRepository = ref.read(authRepositoryProvider);
     state = const AsyncLoading();
     state = await AsyncValue.guard(() => authRepository.signOut());
   }
 }
-
-final accountScreenControllerProvider = StateNotifierProvider.autoDispose<
-    AccountScreenController, AsyncValue<void>>((ref) {
-  final authRepository = ref.watch(authRepositoryProvider);
-  return AccountScreenController(
-    authRepository: authRepository,
-  );
-});
